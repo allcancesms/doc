447cdcb3287c82a8f92b10be40e0a2dbc10628bf
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-02-12 11:57:24 -0300

    feat: alternancia de img com o tema

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,32 @@
 import DefaultTheme from 'vitepress/theme'
+import {onMounted} from "vue";
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  setup() {
+    const defaultSetup = DefaultTheme.setup ? DefaultTheme.setup() : undefined;
+
+    const setImg = (button, img) => {
+      console.log(button.getAttribute('title'))
+      if (!button.getAttribute('title').includes('dark')) img.setAttribute('src', 'allcance-logo-white.png');
+      else img.setAttribute('src', 'allcance-logo-black.png');
+    }
+
+    onMounted(() => {
+      const img = document.querySelector('.VPNavBarTitle img:last-child');
+      setTimeout(() => {setImg(document.querySelector('.VPSwitch.VPSwitchAppearance'), img)}, 1000);
+
+      document.addEventListener("click", (event) => {
+        const button = event.target.closest('.VPSwitch.VPSwitchAppearance');
+        if (button) {
+          button.addEventListener('click', () => {
+            setImg(button, img)
+          })
+        }
+      });
+    });
+
+    return defaultSetup;
+  },
+};
